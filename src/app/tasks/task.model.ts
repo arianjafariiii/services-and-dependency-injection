c6a88dbfa56ced038a58e1b0bbc7144cc3c0483b
ArianJafari <arian.jafari2233@gmail.com>
2025-12-29 14:52:23 +0330

    Preparing a non class value for injection

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,5 +1,25 @@
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
 
+export const taskStatusOptions: {}[] = [
+  {
+    value: 'open',
+    taskStatus: 'OPEN',
+    text: 'Open'
+  },
+  {
+    value: 'in-progress',
+    taskStatus: 'IN_PROGRESS',
+    text: 'In-Progress'
+  },
+  {
+    value: 'done',
+    taskStatus: 'DONE',
+    text: 'Complete'
+  },
+  
+]
+
+
 export interface Task {
   id: string;
   title: string;
